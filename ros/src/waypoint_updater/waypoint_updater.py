f6070d275d346d28b567c4ae353d0c3b92733857
xiaowang <wangxiaonku@gmail.com>
2017-10-14 02:28:30 -0700

    test on simple waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -22,7 +22,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+UPDATE_RATE = 10
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -33,16 +33,43 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
+        #rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_waypoint_cb)
+
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
         self.base_waypoints = None
+        self.msg_seq = 0
+        self.current_pose = None
 
-        rospy.spin()
 
 
-    def next_waypoint(self, position):
+        rate = rospy.Rate(UPDATE_RATE)
+        while not rospy.is_shutdown():
+        	self.loop()
+        	rate.sleep()
+
+    def loop(self):
+    	if self.base_waypoints is not None and self.current_pose is not None:
+    		next_wp = self.next_waypoint(self.current_pose)
+    		num_base_wps = len(self.base_waypoints)
+    		wp_idx = [idx%num_base_wps for idx in range(next_wp, next_wp+LOOKAHEAD_WPS)]
+    		final_waypoints = [self.base_waypoints[wp] for wp in wp_idx]
+    		self.publish_msg(final_waypoints)
+
+
+    def publish_msg(self, final_waypoints):
+    	final_waypoint_msg = Lane()
+    	final_waypoint_msg.header.seq = self.msg_seq
+    	final_waypoint_msg.header.stamp = rospy.Time.now()
+    	final_waypoint_msg.header.frame_id = '/world'
+    	final_waypoint_msg.waypoints = final_waypoints
+    	self.final_waypoints_pub.pub(final_waypoint_msg)
+    	self.msg_seq += 1
+
+
+    def next_waypoint_ori(self, position):
         waypoints_list = self.base_waypoints.waypoints
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
         min_dist = 10000
@@ -74,24 +101,26 @@ class WaypointUpdater(object):
 
     def pose_cb(self, msg):
         # TODO: Implement
-        position = msg.pose.position
-        rospy.loginfo("current_car_position: %s, %s, %s", position.x, position.y, position.z)
-        next_wp = self.next_waypoint(position)
-        next_wp_p = self.base_waypoints.waypoints[next_wp].pose.pose.position
-        rospy.loginfo('next_waypoint: %s, %s, %s', next_wp_p.x, next_wp_p.y,
-        next_wp_p.z)
-        base_waypoints_length = len(self.base_waypoints.waypoints)
-        ahead_waypoints = []
-        for i in range(LOOKAHEAD_WPS):
-            ahead_waypoints.append(self.base_waypoints.waypoints[(next_wp + i) % base_waypoints_length])
-        lane = Lane()
-        lane.waypoints = ahead_waypoints
-        self.final_waypoints_pub.publish(lane)
+        self.current_pose = msg.pose
+        #rospy.loginfo("current_car_position: %s, %s, %s", position.x, position.y, position.z)
+        #next_wp = self.next_waypoint(position)
+        #next_wp_p = self.base_waypoints.waypoints[next_wp].pose.pose.position
+        #rospy.loginfo('next_waypoint: %s, %s, %s', next_wp_p.x, next_wp_p.y,
+        #next_wp_p.z)
+        #base_waypoints_length = len(self.base_waypoints.waypoints)
+        #ahead_waypoints = []
+        #for i in range(LOOKAHEAD_WPS):
+        #    ahead_waypoints.append(self.base_waypoints.waypoints[(next_wp + i) % base_waypoints_length])
+        #lane = Lane()
+        #lane.waypoints = ahead_waypoints
+        #
+        #self.final_waypoints_pub.publish(lane)
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
         # base waypoints is only published once, so it should be stored here
-        self.base_waypoints = waypoints
+        if self.base_waypoints is None:
+        	self.base_waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -115,6 +144,35 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
+    def dist(self, p1, p2):
+    	dx = p1.x - p2.x
+    	dy = p1.y - p2.y
+    	dz = p1.z - p2.z 
+    	return math.sqrt(dx*dx + dy*dy+dz*dz)
+
+    def closest_waypoint(self, pose):
+    	closest_dist = float('inf')
+    	closest_wp = 0
+    	for i in range(len(self.base_waypoints)):
+    		dist = self.dist(pose.pose.position, self.base_waypoints[i].pose.pose.position)
+    		if dist < closest_dist:
+    			closest_dist = dist
+    			closest_wp = i
+    	return closest_wp
+
+
+    def next_waypoint(self, pose):
+    	closest_wp = self.closest_waypoint(pose)
+    	cw_x = self.base_waypoints[closest_wp].pose.pose.position.x
+    	cw_y = self.base_waypoints[closest_wp].pose.pose.position.y
+    	heading = math.atan2((cw_y-pose.position.y),(cw_x-pose.position.x))
+
+    	quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    	_,_, theta = tf.transformations.euler_from_quaternion(quaternion)
+    	angle = abs(theta-heading)
+    	if angle > math.pi/4:
+    		closest_wp += 1
+    	return closest_wp
 
 if __name__ == '__main__':
     try:
