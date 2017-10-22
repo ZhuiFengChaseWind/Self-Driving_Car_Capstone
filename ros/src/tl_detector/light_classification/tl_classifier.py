758f4e385f047f2c016670e9e81f5af9164249f9
ROS <ros@ros-indigo-desktop-trusty64>
2017-10-23 00:51:40 +0800

    Add code for return traffic light colour type

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,12 @@
 from styx_msgs.msg import TrafficLight
+from keras.models import load_model
+import cv2
+import numpy as py
 
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
+    def __init__(self, model_path):
+        #TODO load classifier        
+        self.model = load_model(model_path)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +19,14 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        self.model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+        img = cv2.imread(image)
+        img = cv2.resize(img,(320,240))
+        img = np.reshape(img,[1,320,240,3])
+
+        classes = model.predict_classes(img)
+        return classes
+        ##return TrafficLight.UNKNOWN
