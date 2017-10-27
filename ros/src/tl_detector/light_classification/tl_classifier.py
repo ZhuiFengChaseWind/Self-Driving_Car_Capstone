a1ebdd32aca870e6b48b124bfa0b32e16ce0a668
Calvin Low <calsaviour@gmail.com>
2017-10-27 14:26:20 +0800

    Add inception model for tl classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -9,9 +9,14 @@ import tensorflow as tf
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        self.graph = tf.get_default_graph()
-        with self.graph.as_default():
-            self.model = load_model("light_classification/models/sim_tl_model.h5")
+        # self.graph = tf.get_default_graph()
+        # with self.graph.as_default():
+        #     self.model = load_model("light_classification/models/sim_tl_model.h5")
+        
+        with tf.gfile.FastGFile("light_classification/models/retrain_inception/retrained_graph.pb", 'rb') as f: 
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -26,10 +31,51 @@ class TLClassifier(object):
         #TODO implement light color prediction
 
         # img = cv2.imread(image)
-        img = image
-        img = cv2.resize(img,(150,150))
-        img = np.reshape(img,[1,150,150,3])
-        with self.graph.as_default():
-            classes = self.model.predict_classes(img)
-            return classes
+        # img = image
+        # img = cv2.resize(img,(150,150))
+        # img = np.reshape(img,[1,150,150,3])
+        # with self.graph.as_default():
+        #     classes = self.model.predict_classes(img)
+        #     return classes
         ##return TrafficLight.UNKNOWN
+
+
+        # ------------------------------------------------------
+        # TrafficLight.msg enum are as below
+
+        # uint8 UNKNOWN=4
+        # uint8 GREEN=2
+        # uint8 YELLOW=1
+        # uint8 RED=0
+
+        # However, tha classifier's label are as below
+        # red = 1
+        # green = 0
+        # yellow = 2
+        # ------------------------------------------------------
+
+        with tf.Session() as sess:
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+            predictions = sess.run(softmax_tensor, \
+                    {'DecodeJpeg/contents:0': image_data})
+
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            prediction = top_k[0]
+            print("The prediction label is ", prediction)
+
+
+            if prediction == TrafficLight.RED:               
+               print("Detected is RED")
+               prediction = TrafficLight.RED
+            elif prediction == TrafficLight.YELLOW:
+                print("Detected is YELLOW")
+                prediction = TrafficLight.YELLOW
+            elif prediction == TrafficLight.GREEN:
+                print("Detected is GREEN")
+                prediction = TrafficLight.YELLOW
+            else:
+                print("Path is clear")
+                prediction == TrafficLight.UNKNOWN
+
+            return prediction
+
